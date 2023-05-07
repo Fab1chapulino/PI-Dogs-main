ffe3702141975ced0f37907cca0a4e43c232c275
fabian <chapulinozeta@gmail.com>
2023-05-07 17:24:05 -0400

    Added some commetns

--- api/src/routes/controllers/getDogByName.js
+++ api/src/routes/controllers/getDogByName.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const { API_KEY } = process.env;
+
 const axios = require("axios");
 const {Dog, Temperament}= require("../../db");
 const applySearch = require('../../utils.js')
 
 module.exports = async (req, res)=>{
     try{
+        //Get query search from req.query
         const {query}= req.query
+        //Searching breeds from api
         const {data}= await axios.get(`https://api.thedogapi.com/v1/breeds/search?q=${query}&&api_key=${API_KEY}`)
         let apiDogs=data.map(dog=>{
             return {
@@ -16,10 +19,15 @@ module.exports = async (req, res)=>{
                 weight:dog.weight.imperial
             }
         })
+        //Only need those with temperaments
         apiDogs = apiDogs.filter( dog => dog.temperaments)
+
+        //Searching breeds from DB
         const dbDogs = await applySearch(query);
+
+        //joining both results
         const dogs = dbDogs.concat(apiDogs);
-        console.log(dogs, "<------dogs")
+
         if(dogs.length){
             res.status(200).json(dogs)
         }else{
